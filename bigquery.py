734b45b2c93c7cda09586146612ca60cb02ba357
EduardoTeranisi <eduardo.teranisi@kanastra.com.br>
2025-02-22 13:31:10 -0300

    feat: window function module

--- bigquery.py
+++ bigquery.py
@@ -18,3 +18,22 @@ table_ref = dataset_ref.table("full");
 table = client.get_table(table_ref);
 
 #schema é a estrutura de uma tabela
+
+#estabelendo limites para a query
+query = """     
+        SELECT score, title
+        FROM `bigquery-public-data.hacker_news.full`
+        WHERE type = "job" 
+        """
+
+# Only run the query if it's less than 1 MB
+ONE_MB = 1000*1000
+safe_config = bigquery.QueryJobConfig(maximum_bytes_billed=ONE_MB)
+
+# Set up the query (will only run if it's less than 1 MB)
+safe_query_job = client.query(query, job_config=safe_config)
+
+# API request - try to run the query, and return a pandas DataFrame
+safe_query_job.to_dataframe()
+
+#ORDER BY is usually the last clause in your query, and it sorts the results returned by the rest of your query.
